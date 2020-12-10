894bbad486783f29c452caccae6e42ae6093f00e
Alex Oliveira <alexoliveira7x@gmail.com>
2020-12-10 16:02:32 -0300

    tired.. later ill back

---
+++ src/components/ExploreContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="job-lists">
+    <div class="job-box">
+      <img src="https://foroalfa.org/imagenes/ilustraciones/g-1.jpg" />
+      <div>
+        <h4>UI designer & UX designer</h4>
+        <span>Google Inc.</span>
+        <hr/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'ExploreContainer',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+.job-box {
+      display: flex;
+}
+.job-box img{
+  max-width: 40px;
+  max-height: 40px;
+}
+</style>
---
+++ src/views/Tabs.vue
@@ -0,0 +1,46 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="compassOutline" />
+          <ion-label>Explore</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="notificationsOutline" />
+          <ion-label>Notification</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="heartOutline" />
+          <ion-label>Liked</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="briefcaseOutline" />
+          <ion-label>Portfolio</ion-label>
+        </ion-tab-button>
+
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
+import { notificationsOutline, heartOutline, briefcaseOutline, compassOutline } from 'ionicons/icons';
+
+export default {
+  name: 'Tabs',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+  setup() {
+    return {
+      notificationsOutline, 
+      heartOutline, 
+      briefcaseOutline,
+      compassOutline
+    }
+  }
+}
+</script>
